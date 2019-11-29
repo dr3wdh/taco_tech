b200e45c9a7349d12b114b488e5727a21aa6122d
Drew Harman <drewharman@gmail.com>
2019-11-29 15:27:05 -0700

    updating for current python modules

--- lights/examples/baseline.py
+++ lights/examples/baseline.py
@@ -1,5 +1,5 @@
 #from rpi_ws281x import *
-from neopixel import *
+from rpi_ws281x import *
 import time
 
 #LED_COUNT = 149
@@ -15,15 +15,11 @@ COLOR_R = 255
 COLOR_G = 255
 COLOR_B = 255
 
-#strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-#strip.begin()
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
 
 try:
     while True:
-        reload(led_config)
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-        strip.begin()
-
         for i in range (0, LED_COUNT):
             strip.setPixelColor(i, Color(COLOR_G,COLOR_R,COLOR_B))
             strip.show()
