6c7860065217360136d333470390d03376bde406
Drew Harman <drewharman@gmail.com>
2019-12-13 21:20:25 -0700

    adding code to baseline example for multiple light strips

--- lights/examples/baseline.py
+++ lights/examples/baseline.py
@@ -2,18 +2,20 @@
 from rpi_ws281x import *
 import time
 
-#LED_COUNT = 149
-LED_COUNT = 38
-LED_PIN = 18
+#LED_COUNT = 106
+LED_COUNT = 10
+#LED_PIN = 18
+LED_PIN = 19
 LED_FREQ_HZ = 800000
 LED_DMA = 10
-LED_BRIGHTNESS = 64
+LED_BRIGHTNESS = 255
 LED_INVERT = False
-LED_CHANNEL = 0
+#LED_CHANNEL = 0
+LED_CHANNEL = 1
 
 COLOR_R = 255
-COLOR_G = 255
-COLOR_B = 255
+COLOR_G = 0
+COLOR_B = 0
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
@@ -21,7 +23,7 @@ strip.begin()
 try:
     while True:
         for i in range (0, LED_COUNT):
-            strip.setPixelColor(i, Color(COLOR_G,COLOR_R,COLOR_B))
+            strip.setPixelColor(i, Color(COLOR_R,COLOR_G,COLOR_B))
             strip.show()
         time.sleep( 1 )
 except KeyboardInterrupt:
